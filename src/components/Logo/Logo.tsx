ad8eeaa68fe8824b468717b2f4851bd78e092cc3
Miroslav <melnik.miros@gmail.com>
2020-02-24 23:24:16 +0300

    Add Logo component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './Logo.css';
+
+const Logo = () => {
+    return (
+        <div className="logo">
+            <div className="logo__img" />
+            <span className="logo__text">Open Circles</span>
+        </div>
+    );
+};
+
+export default Logo;
